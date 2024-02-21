f6ae5c2f3ac4c192d31062938b3886a778a718f6
Zfike <97480418+Zfike@users.noreply.github.com>
2024-02-20 20:08:27 -0600

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,7 +10,7 @@
         </v-col>
         <v-col class="pa-0">
           <v-img
-            :src="`/public/oc-logo-maroon.png`"
+            :src="`/oc-logo-maroon.png`"
             height="60"
             width="60"
             class="logo"
