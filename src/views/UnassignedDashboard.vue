662c1428d07ee2b0d71e8a4f9f4a87264796bd22
Solomon <solomon.granger@eagles.oc.edu>
2024-02-26 01:20:24 -0600

    Added maintain people functionality, Added people to maintain menu, adjusted router to handle needs, updated userDashboard and adminDashboard for demo examples

--- src/views/UnassignedDashboard.vue
+++ src/views/UnassignedDashboard.vue
@@ -13,7 +13,6 @@
             you a role before you move forward. Either contact an administrator in person 
             or use the button below to notify an administrator.
       </p>
-      <v-btn color="secondary" @click="">Notify Administator // This button has no function</v-btn>
           <!-- Additional content for unassigned dashboard can go here -->
         </v-card-text>
       </v-card>
