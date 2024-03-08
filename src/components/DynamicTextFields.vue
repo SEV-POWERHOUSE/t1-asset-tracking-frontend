05f00cf59c6320e75e10a61036f970da4eef1f67
Zfike <97480418+Zfike@users.noreply.github.com>
2024-03-07 18:54:28 -0600

    commit before I make an experimental branch

--- src/components/DynamicTextFields.vue
+++ src/components/DynamicTextFields.vue
@@ -1,21 +1,63 @@
 <script setup>
-import { ref, toRefs, watch } from "vue";
+import { ref, toRefs, watch } from "vue"
 
+// Validation Rules
+const rules = {
+  required: (value) => !!value || "Required.",
+  maxDescLength: (value) => value.length <= 255,
+  maxNameLength: (value) => value.length <= 50,
+  serialNumberLength: (value) => value.length <= 20,
+}
 // Define props
 const props = defineProps({
     selectedTypeId: { 
         required: true
+    },
+    formData: {
+        required: true
     }
-});
+})
 
-// Destructure the selectedTypeId prop
-const { selectedTypeId } = toRefs(props);
+// Destructure the selectedTypeId and formData props
+const { selectedTypeId, formData } = toRefs(props)
 
-const typeID = ref(selectedTypeId.value);
+const typeID = ref(selectedTypeId.value)
 
 watch(selectedTypeId, (newValue, oldValue) => {
     console.log("passed prop ID:", selectedTypeId.value)
     typeID.value = newValue;
+})
+
+const textFieldValue = ref('')
+
+const submitForm = () => {
+    const dataToEmit = {
+        name: "Manufacturer",
+        data: textFieldValue.value
+    }
+    
+    // Emit an event to pass data back to parent
+    emit('formData', dataToEmit)
+}
+
+// Function to populate text field with data from formData
+const populateTextField = (label) => {
+    if (formData.value[label]) {
+        textFieldValue.value = formData.value[label]
+    } else {
+        textFieldValue.value = '' // Clear the text field if no data found
+    }
+};
+
+// Call populateTextField when component is mounted or selectedTypeId changes
+onMounted(() => {
+    populateTextField('Manufacturer') // Adjust the label as needed
+});
+
+watch(selectedTypeId, (newValue, oldValue) => {
+    console.log("passed prop ID:", selectedTypeId.value)
+    typeID.value = newValue
+    populateTextField('Manufacturer')
 });
 
 </script>
@@ -23,15 +65,15 @@ watch(selectedTypeId, (newValue, oldValue) => {
 <template>
     <div v-if="selectedTypeId=='2'">
       <!-- Display different text fields based on selected type -->
-        <v-row>
-            <v-col cols="12">
-                <v-text-field
-                    label="Manufacturer"
-                    v-model="newSerializedAsset.serializedNumber"
-                    :rules="[rules.required, rules.serialNumberLength]"
-                    maxlength="20"
-                    counter
-                ></v-text-field>
-            </v-col>
+        <v-text-field
+            label="Manufacturer"
+            v-model="textFieldValue.value"
+            :rules="[rules.maxNameLength, rules.re]"
+            maxlength="20"
+            counter
+        ></v-text-field>
     </div>
 </template>
+
+
+<!-- Love, Zane -->
